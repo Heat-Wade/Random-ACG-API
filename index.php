1564b9fe52ea3a7e1b6e94aa2bbc4c984943ae4b
Heat-Wade <79273007+Heat-Wade@users.noreply.github.com>
2021-03-01 16:31:55 +0800

    2

--- index.php
+++ index.php
@@ -3,37 +3,37 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		<title>随机二次元图片API - 三秋API接口站</title>
-		<meta name="keywords" content="三秋API接口站,图库,二次元图片,二次元API,动漫图片API,图片API">
+		<title>随机二次元图片API - Nanami's 背景API接口站</title>
+		<meta name="keywords" content="Nanami's背景API接口站">
 		<meta name="description" content="随机二次元图片API,随机动漫壁纸,每天刷一刷，每次不一样。">
 		<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="css/main.css">
 	</head>
 	<body>
 		<div>
-			<p algin=“center” class="title title2">随机二次元图片API - 三秋API接口站</p>
+			<p algin=“center” class="title title2">随机二次元图片API - Nanami's背景API接口站</p>
 		</div>
 		<div class="container">
 			<div class="well">
-				<p>目前系统共收录 3k8+ 张图片 API最后更新时间:2020-07-14</p>
-				<p>图片均为https，采用又拍云存储，高速访问</p>
+				<p>目前系统共收录 80+ 张图片 API最后更新时间:2021-03-01</p>
+				<p>图片采用gitee等多种方式存储</p>
 				<p>自判断二次元API基本调用格式：</p>
 				<pre><a href="<?php $_SERVER['HTTP_HOST'] ?>/api.php" target="_blank"><?php echo 'http://'.$_SERVER['HTTP_HOST'] ?>/api.php</a></pre>
-				<p>PC随机二次元API</p>
+				<p>（横屏）PC端随机二次元API</p>
 				<pre><a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'] ?>/pc.php" target="_blank"><?php echo 'http://'.$_SERVER['HTTP_HOST'] ?>/pc.php</a></pre>
-				<p>PE随机二次元API</p>
+				<p>（竖屏）手机端随机二次元API</p>
 				<pre><a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'] ?>/pe.php" target="_blank"><?php echo 'http://'.$_SERVER['HTTP_HOST'] ?>/pe.php</a></pre>
 				<p>请求方式</p>
 				<pre>get/post</pre>
 				<p>返回格式</p>
 				<pre>json/images</pre>
-				<p>一叶三秋</p>
-				<pre><a href="http://ghser.com" target="_blank">http://ghser.com</a></pre>
+				<p>博客</p>
+				<pre><a href="https://histion.github.io" target="_blank">https://histion.github.io</a></pre>
 			</div>
 		</div>
 		<div class="bottom">
 
-                    <p>Copyright © 2020 <a href="https://api.ghser.com/" target="_blank" class="one-pan-link-mark">三秋API接口站</a></p>
+                    <p>China © 2021 <a href="https://histion.github.io" target="_blank" class="one-pan-link-mark">博客</a></p>
 		</div>
 	</body>
 </html>
